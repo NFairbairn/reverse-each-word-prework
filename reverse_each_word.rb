2a4197e0aee8853c7bba131b6971a66923cdd613
Noah Fairbairn <noahfairbairn@gmail.com>
2019-01-29 19:25:19 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
   array = string.split(" ")
-  new_arr = array.collect {|x| x.reverse}
-  new_arr.join(" ")
+  new_arr = array.collect.join(" ") {|x| x.reverse}
+  #new_arr.join(" ")
 end
